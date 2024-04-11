7c8ff35511c9aaefa29a32c2a7aa19d1b4551b43
Raam <raam.sanghani@gmail.com>
2024-04-11 13:46:13 -0500

    finished fleshing out core functionality for HashMap class methods! Last thing to do is to return to set(key,value) and add logic to check for capacity and grow our bucket size accordingly

--- hashMap.js
+++ hashMap.js
@@ -1,7 +1,9 @@
 import { LinkedList, Node } from "./linkedList.js";
 
 class HashMap {
+
     constructor() {
+
         this.capacity = 25;
         this.load_factor = .75;
         this.map = [];
@@ -9,6 +11,7 @@ class HashMap {
     }
 
     hash(key){
+
         let hashCode = 0;
       
         const primeNumber = 31;
@@ -22,9 +25,9 @@ class HashMap {
     set(key, value){
         
         const index = this.hash(key) % this.capacity;
-        console.log(index) 
 
         if (this.map[index]) { 
+
             if (this.map[index].size === 1) {
                 if (this.map[index].head.value.key === key) { 
                     this.map[index].head.value.value = value
@@ -37,11 +40,9 @@ class HashMap {
                 return
             }
 
-            
             let current = this.map[index].head
             let i = 0;
 
-            
             while (i < this.map[index].size) {
                 if (current.value.key === key) {
                     current.value.value = value
@@ -57,10 +58,7 @@ class HashMap {
             })
 
             return
-
         }
-
-       
 
         const bucketContents = new LinkedList()
         this.map[index] = bucketContents
@@ -78,7 +76,6 @@ class HashMap {
         const index = this.hash(key) % this.capacity
         const bucket = this.map[index]
 
-        
         if (!bucket) {
             console.error(`${key} not found, try set(key, value) first`)
             return
@@ -98,14 +95,10 @@ class HashMap {
             return
         }
 
-       
-
         if (bucket.head.value.key != key) {
             console.error(`${key} not found, try set(key, value) first`)
             return
         }
-
-
 
         return bucket.head.value.value
     }
@@ -143,7 +136,7 @@ class HashMap {
     
         if (!bucket) {
             console.error(`Cannot remove key: ${key} - does not exist`)
-            return
+            return false
         }
 
         if (bucket.size > 1) {
@@ -156,7 +149,7 @@ class HashMap {
                     console.log(`Removing key:value pair ${key} : ${current.value.value} from the bucket...`)
                     bucket.removeAt(i)
                     console.log(`Successfully removed ${key} from bucket!`)
-                    return
+                    return true
                 }
                 
                 current = current.nextNode
@@ -164,36 +157,141 @@ class HashMap {
             }
             
             console.error(`Cannot remove key: ${key} - does not exist`)
-            return
+            return false
         }
-        
-
-
         
         if (bucket.head.value.key != key) {
             console.error(`Cannot remove key: ${key} - does not exist`)
-            return
+            return false
         }
-
-        
 
         bucket.pop()
         console.log(`Removed key:value pair associated with: ${key}`)
-        return
+        return true
+    }
 
+    length() {
+
+        let totalKeys = 0;
+        if (!this.map) {return totalKeys}
+
+        for (const bucket of this.map) {
+            if (!bucket) continue
+
+            totalKeys += bucket.size
+        }
+
+        return totalKeys
+    }
+
+    clear(){
+        this.map.fill(null);
+    }
+
+    keys(){
+        if (!this.map){
+            return []
+        }
+        //If the code gets to this point, then our hashMap contains something.
+
+        const keys = []
+
+        //Let's loop through each bucket
+        for (const bucket of this.map) {
+            //Is the bucket empty? Then skip!
+            if (!bucket) continue
+            //At this point in the script, the bucket is not empty
+            //Does the bucket have more than one item? KEEP IN MIND THAT EACH BUCKET IS A LINKED LIST
+            
+            if (bucket.size > 1) {
+                let current = bucket.head
+                let i = 0
+
+                while (i < bucket.size) {
+                    keys.push(current.value.key)
+                    current = current.nextNode
+                    i++
+                }
+                continue
+            }
+
+            //at this point, if the script is running without getting caught in the above checks, the bucket only has one item
+
+            keys.push(bucket.head.value.key)
+        }
+
+        return keys
 
     }
 
-    length() {}
+    values(){
+        //Very similar to the keys() function
+        if (!this.map){
+            return []
+        }
+        //If the code gets to this point, then our hashMap contains something.
 
-    clear(){}
+        const values = []
 
-    keys(){}
+        //Let's loop through each bucket
+        for (const bucket of this.map) {
+            //Is the bucket empty? Then skip!
+            if (!bucket) continue
+            //At this point in the script, the bucket is not empty
+            //Does the bucket have more than one item? KEEP IN MIND THAT EACH BUCKET IS A LINKED LIST
+            
+            if (bucket.size > 1) {
+                let current = bucket.head
+                let i = 0
 
-    values(){}
+                while (i < bucket.size) {
+                    values.push(current.value.value)
+                    current = current.nextNode
+                    i++
+                }
+                continue
+            }
 
-    entries(){}
+            //at this point, if the script is running without getting caught in the above checks, the bucket only has one item
 
+            values.push(bucket.head.value.value)
+        }
+
+        return values
+
+    }
+
+    entries(){
+
+        let entries = []
+
+        if (!this.map) {return entries} //If caught in this check, the method will return an empty array
+
+        //If the code runs at this point, then there is something in the HashMap
+        for (const bucket of this.map) {
+            
+            if (!bucket) continue
+            
+            if (bucket.size > 1) {
+
+                let current = bucket.head
+                let i = 0
+
+                while (i < bucket.size) {
+
+                    entries.push(current.value)
+                    current = current.nextNode
+                    i++
+                }
+
+                continue
+            }
+
+            entries.push(bucket.head.value)
+        }
+
+        return entries
+    }
 }
 
 export default HashMap;
--- index.js
+++ index.js
@@ -1,26 +1,49 @@
 import HashMap from "./hashMap.js";
+//This is my testing script to verify HashMap class functionality. 
 
 
+//Start our map
 const hasheesh = new HashMap
 
 hasheesh.set("Carlos", "yo yo yo")
 hasheesh.set("Carla", "Hey hey hey")
 
+//Same HashCodes (based on a 25 capacity and .75 load factor) add a nextNode to the linkedList (bucket)
 hasheesh.set("aa", "This is value #1")
 hasheesh.set("az", "This is value #2")
 
-console.log(hasheesh.map[4].head)
+console.log(hasheesh.keys())
+console.log(hasheesh.values())
+console.log(hasheesh.entries())
 
-hasheesh.set("az", "This is value #3")
-hasheesh.remove("aa")
+// console.log(hasheesh.map)
+// console.log(hasheesh.length())
+// console.log(hasheesh.map.length)
+// hasheesh.clear()
+// console.log("******* VALUES HAVE BEEN CHANGED*******")
 
-console.log("******* VALUES HAVE BEEN CHANGED*******")
+// console.log(hasheesh.map)
+// console.log(hasheesh.length())
+// console.log(hasheesh.map.length)
 
-console.log(hasheesh.map[4].head)
+// console.log(hasheesh.map[4].head)
+
+// hasheesh.set("az", "This is value #3")
 
 
-console.log("*********THE ENDING HASHMAP********")
-console.log(hasheesh.map)
+// hasheesh.remove("aa")
+
+// console.log("******* VALUES HAVE BEEN CHANGED*******")
+
+// console.log(hasheesh.map[4].head)
+
+
+// console.log("*********THE ENDING HASHMAP********")
+// console.log(hasheesh.map)
+
+// console.log(hasheesh.length())
+
+
 
 
 // console.log(hasheesh.map[22].head)
